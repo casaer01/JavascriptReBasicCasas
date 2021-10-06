a6c464e63165ff86c079e923b2c29ba8e309097a
Eros Casas <ecasas182015@gmail.com>
2021-10-06 18:48:19 -0500

    video 25

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,8 +1,10 @@
 const defaultResult = 0;
-let currentResult = 0;
+let currentResult = defaultResult;
 
 currentResult = currentResult + 10;
 
-let calculationDescription = '( ' + currentResult + ' + 10) * 3 / 2 - 1';
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+let errorMessage = 'An error \n' +
+    ' eccurred';
 
-outputResult(currentResult, calculationDescription);
+outputResult(currentResult, errorMessage);
