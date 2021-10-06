14754bb6e4078f95b57dd3c2ad9ef95a9cbfab8e
Eros Casas <ecasas182015@gmail.com>
2021-10-06 18:37:25 -0500

    ended on video 25

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,3 +1,8 @@
+const defaultResult = 0;
 let currentResult = 0;
 
 currentResult = currentResult + 10;
+
+let calculationDescription = '( ' + currentResult + ' + 10) * 3 / 2 - 1';
+
+outputResult(currentResult, calculationDescription);
