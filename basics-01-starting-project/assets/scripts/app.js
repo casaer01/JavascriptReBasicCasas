1da44a2f427fd014bd59cdbdd69f7de37e4a1fda
Eros Casas <ecasas182015@gmail.com>
2021-10-07 17:16:08 -0500

    stopped at 34

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,10 +1,12 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-currentResult = currentResult + 10;
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+currentResult = add(10, 23);
 
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
-let errorMessage = 'An error \n' +
-    ' eccurred';
 
 outputResult(currentResult, errorMessage);
