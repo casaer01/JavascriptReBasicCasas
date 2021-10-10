212c4961b3f64bd840495ba58f89d6f97d347a2e
Eros Casas <ecasas182015@gmail.com>
2021-10-10 15:13:11 -0500

    tested out user input

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,11 +1,47 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getUserNum() {
+    return parseInt(userInput.value);
+}
+
+function createAndWriteLog(operator, resultBefore, calcNumber) {
+    const calcDescription = `${resultBefore} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription)
+}
+
 function add() {
-    currentResult = currentResult + parseInt(userInput.value);
-    outputResult(currentResult, '');
+    const userNum = getUserNum();
+    const initialResult = userNum;
+    currentResult = currentResult + userNum;
+    createAndWriteLog("+", initialResult, userNum);
+}
+
+function subtract() {
+    const userNum = getUserNum();
+    const initialResult = userNum;
+    currentResult = currentResult - userNum;
+    createAndWriteLog("-", initialResult, userNum);
+}
+
+function multiply() {
+    const userNum = getUserNum();
+    const initialResult = userNum;
+    currentResult = currentResult * userNum;
+    createAndWriteLog("*", initialResult, userNum);
+}
+
+function divide() {
+    const userNum = getUserNum();
+    const initialResult = userNum;
+    currentResult = currentResult / userNum;
+    createAndWriteLog("/", initialResult, userNum);
 }
 
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
+
 
 
